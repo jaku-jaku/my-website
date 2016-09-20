f3e903d4f2c8d88917282a2af9b87fd499bf45fd
Jack Xu <jack19980118@gmail.com>
2016-09-20 14:34:07 -0400

    s

--- js/script.js
+++ js/script.js
@@ -34,7 +34,9 @@ $(function() {
     event.preventDefault();
   });
 });
-document.getElementById("#portfolioModal1").style.paddingLeft = "0px";
+
+//
+$(function() {$('html').getElementById("#portfolioModal1").style.paddingLeft = "0px"});
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
   if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
