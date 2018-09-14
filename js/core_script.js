a5fadb4409e55e770f243f411a61427a929f6d6b
Jack Xu <jack19980118@gmail.com>
2018-09-14 00:55:22 -0400

    + Auto-arrange projects by alphabetic id_name + Auto-generate cards and jumping < - x - > through different projects

--- js/core_script.js
+++ js/core_script.js
@@ -15,80 +15,149 @@ $(function(){
 
 
   /*----------------------------------------------------*/
-  /* AUTO portfolio card gen
+  /* Load json file, and sort
   ------------------------------------------------------ */
-$(function () {
-    $.getJSON("sub_mod/pfo_projects.json",
-        function (json) {
-            var pfo_elem;
-            for (var i = 0; i < json.length; i++) {
-                pfo_elem = "";
-                pfo_elem += " <li class=\"pfo-item\">\n" +
-                    "<div class=\"card col-lg-4 col-md-4 col-xs-6 col-sm-6\" id=\"" + json[i].id_name + "\">" +
-                    "    <span class=\"card-pic\">\n" +
-                    "      <div class = \"card-pic-crop\" >\n" +
-                    "      <img src=\""+ json[i].img_directory + json[i].img_cover + "\"/>\n" +
-                    "      </div> \n" +
-                    "      <div class=\"card-description\">\n" +
-                    "      <div class=\"card-des-title\">\n" +
-                            json[i].title+
-                    "      </div>\n" +
-                    "      <div class=\"card-des-explain\">\n" +
-                            json[i].description+
-                    "      </div>\n" +
-                    "      </div>\n" +
-                    "    </span>\n" +
-                    "</div>\n" +
-                    "</li>";
+  var G_j_pfo_projs = null;
+  function sortResults(j_arr, prop, asc) {
+      return j_arr.sort(function(a, b) {
+          if (asc) return (a[prop] > b[prop]);
+          else return (b[prop] > a[prop]);
+      });
+  }
 
-                $('#pfo-bundle').append(pfo_elem);
-            }
-        });
-});
-
-
-  $(document).on('click', '.card', function () {
-      var id = this.id;
-      console.log("Clicked = "+id);
-
-      var modal_src_imgs;
-      modal_src_imgs = "<div class=\"carouselCard\">\n" +
-          "                            <div class=\"owl-demo owl-carousel img-comment\" >";
-
+  //Load json and sort json and generate pfo cards
+  $(function () {
       $.getJSON("sub_mod/pfo_projects.json",
           function (json) {
-              for (var i = 0; i < json.length; i++) {
-
-                  if(json[i].id_name == id)
-                  {
-                      var j_items = json[i].more_imgs;
-                      for (var j = 0; j < j_items.length; j++)
-                      {
-                          modal_src_imgs += "<div><img src=\"" + json[i].img_directory + j_items[j] + "\"><p>"+ json[i].more_imgs_title[j] +"</p></div>\n";
-                      }
-
-                      modal_src_imgs += " </div>\n" +
-                          "                        </div>";
-                      console.log(modal_src_imgs);
-                      $('#modal-templ-imgs').html(modal_src_imgs);
-
-                      $(".owl-demo").owlCarousel({
-                          navigation:true,
-                          navigationText:["<span class='angle'></span>","<span class='angle'></span>"],
-                          singleItem : true,
-                          autoHeight : true,
-                          transitionStyle:"fade"
-                      });
-
-                      $('#portfolioModalTemplate').modal('show');
-                      break;
-                  }
+              G_j_pfo_projs = json;
+              // console.log(G_j_pfo_projs);
+              if(G_j_pfo_projs != null)
+              {
+                  G_j_pfo_projs = sortResults(G_j_pfo_projs, "id_name", true);
+                  gen_pfo_cards(G_j_pfo_projs);
               }
-          }
-      );
-
+          });
   });
 
+
+  /*----------------------------------------------------*/
+  /* AUTO portfolio card gen
+  ------------------------------------------------------ */
+  function gen_pfo_cards(_G_j_pfo_projs){
+      var pfo_elem;
+      for (var i = 0; i < _G_j_pfo_projs.length; i++) {
+          pfo_elem = "";
+          pfo_elem += " <li class=\"pfo-item\">\n" +
+              "<div class=\"card col-lg-4 col-md-4 col-xs-6 col-sm-6\" id=\"" + _G_j_pfo_projs[i].id_name + "\">" +
+              "    <span class=\"card-pic\">\n" +
+              "      <div class = \"card-pic-crop\" >\n" +
+              "      <img src=\""+ _G_j_pfo_projs[i].img_directory + _G_j_pfo_projs[i].img_cover + "\"/>\n" +
+              "      </div> \n" +
+              "      <div class=\"card-description\">\n" +
+              "      <div class=\"card-des-title\">\n" +
+              _G_j_pfo_projs[i].title+
+              "      </div>\n" +
+              "      <div class=\"card-des-explain\">\n" +
+              _G_j_pfo_projs[i].description+
+              "      </div>\n" +
+              "      </div>\n" +
+              "    </span>\n" +
+              "</div>\n" +
+              "</li>";
+
+          $('#pfo-bundle').append(pfo_elem);
+      }
+  }
+  /*----------------------------------------------------*/
+  /* AUTO portfolio modal real-time gen
+  ------------------------------------------------------ */
+  var G_cur_pfo_id_name = "";
+  //find item index in json list
+  function indexOfItemInJSON (_item_name, _item_val, _json_list){
+      return _json_list.findIndex(function(__item, _i){
+          return __item[_item_name]===_item_val;
+      });
+  }
+
+  function gen_pfo_modal(_id_name, offset)
+  {
+      // Use preloaded json to construct page dynamically
+      if(G_j_pfo_projs != null) {
+          var i = indexOfItemInJSON("id_name",_id_name,G_j_pfo_projs);
+          if(i != -1) {
+              i = i + offset; //offsetting the selection for prev/next
+              i = i <= -1 ? (G_j_pfo_projs.length-1) : i; //-ve prev
+              i = i >= G_j_pfo_projs.length ? 0 : i; //next is 0
+
+              G_cur_pfo_id_name = G_j_pfo_projs[i].id_name; //save id_name globally
+              var modal_html_buffer;
+
+              modal_html_buffer = "<h1>" + G_j_pfo_projs[i].title + "<h1>";
+              $('#modal-templ-title').html(modal_html_buffer);
+
+              modal_html_buffer = "<div class=\"carouselCard\">\n" +
+                  "                            <div class=\"owl-demo owl-carousel img-comment\" >";
+              //load demo images
+              var j_items = G_j_pfo_projs[i].more_imgs;
+              var j = 0;
+              for (j = 0; j < j_items.length; j++) {
+                  modal_html_buffer += "<div><img src=\"" + G_j_pfo_projs[i].img_directory
+                      + j_items[j] + "\"><p>" + G_j_pfo_projs[i].more_imgs_title[j] + "</p></div>\n";
+              }
+
+              modal_html_buffer += " </div>\n" +
+                  "                        </div>";
+              // console.log(modal_html_buffer);
+              $('#modal-templ-imgs').html(modal_html_buffer);
+
+              //load details
+              j_terms = G_j_pfo_projs[i].tags;
+              modal_html_buffer = "<p><strong>Position: </strong>" + G_j_pfo_projs[i].position + "</p>\n" +
+                  "                            <p><strong>Category: </strong>";
+              for (j = 0; j < j_terms.length - 1; j++) {
+                  modal_html_buffer += j_terms[j] + ", ";
+              }
+              modal_html_buffer += j_terms[j];
+              modal_html_buffer += "</p>\n" +
+                  " <p><strong>View Code: </strong> <i class=\"fa fa-github\" style=\"padding-right:2px;\"></i><a href=\"" +
+                  G_j_pfo_projs[i].code_link + "\" target=\"_blank\">"
+                  + G_j_pfo_projs[i].code_link + "</a></p>\n";
+              $('#modal-templ-details').html(modal_html_buffer);
+
+              //load descriptions Paragraphs
+              modal_html_buffer = "<h5 class=\"info-font\">Project Description</h5>\n";
+              j_terms = G_j_pfo_projs[i].paras_title;
+              for (j = 0; j < j_terms.length; j++) {
+                  modal_html_buffer += "<div  class=\"project-head4\"><h4>" + j_terms[j] + "</h4></div>\n";
+                  modal_html_buffer += "<p class=\"project-para\">\n" + G_j_pfo_projs[i].paras[j] + "</p>\n";
+              }
+              $('#modal-templ-description').html(modal_html_buffer);
+
+              //Update owl image
+              $(".owl-demo").owlCarousel({
+                  navigation: true,
+                  navigationText: ["<span class='angle'></span>", "<span class='angle'></span>"],
+                  singleItem: true,
+                  autoHeight: true,
+                  transitionStyle: "fade"
+              });
+
+              //Display the modal
+              $('#portfolioModalTemplate').modal('show');
+          }
+      }
+  }
+  $(document).on('click', '.card', function () {
+      var id = this.id;
+      // console.log("Clicked = "+id);
+      gen_pfo_modal(id, 0);
+  });
+  $(document).on('click', '.prevp', function () {
+      gen_pfo_modal(G_cur_pfo_id_name, -1);
+  });
+  $(document).on('click', '.nextp', function () {
+      gen_pfo_modal(G_cur_pfo_id_name, +1);
+  });
 
   /*----------------------------------------------------*/
   /* Navbar Smooth Scroll
