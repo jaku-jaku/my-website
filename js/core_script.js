67131deabd857593bcbb1d86f4c9d54b7d1f4dfc
Jack Xu <jack19980118@gmail.com>
2018-09-15 01:13:17 -0400

    + Fix Filtering System for projects . with animation

--- js/core_script.js
+++ js/core_script.js
@@ -18,6 +18,7 @@ $(function(){
   /* Load json file, and sort
   ------------------------------------------------------ */
   var G_j_pfo_projs = null;
+  var G_j_pfo_projs_filtered= null;
   function sortResults(j_arr, prop, asc) {
       return j_arr.sort(function(a, b) {
           if (asc) return (a[prop] > b[prop]);
@@ -30,11 +31,12 @@ $(function(){
       $.getJSON("sub_mod/pfo_projects.json",
           function (json) {
               G_j_pfo_projs = json;
-              // console.log(G_j_pfo_projs);
+              // console.log(G_j_pfo_projs_filtered);
               if(G_j_pfo_projs != null)
               {
                   G_j_pfo_projs = sortResults(G_j_pfo_projs, "id_name", true);
-                  gen_pfo_cards(G_j_pfo_projs);
+                  G_j_pfo_projs_filtered = G_j_pfo_projs;
+                  gen_pfo_cards(G_j_pfo_projs_filtered);
               }
           });
   });
@@ -43,30 +45,31 @@ $(function(){
   /*----------------------------------------------------*/
   /* AUTO portfolio card gen
   ------------------------------------------------------ */
-  function gen_pfo_cards(_G_j_pfo_projs){
+  function gen_pfo_cards(_G_j_pfo_projs_filtered){
       var pfo_elem;
-      for (var i = 0; i < _G_j_pfo_projs.length; i++) {
-          pfo_elem = "";
+      pfo_elem = "";
+      $('#pfo-bundle').slideUp(300);
+      for (var i = 0; i < _G_j_pfo_projs_filtered.length; i++) {
           pfo_elem += " <li class=\"pfo-item\">\n" +
-              "<div class=\"card col-lg-4 col-md-4 col-xs-6 col-sm-6\" id=\"" + _G_j_pfo_projs[i].id_name + "\">" +
+              "<div class=\"card col-lg-4 col-md-4 col-xs-6 col-sm-6\" id=\"" + _G_j_pfo_projs_filtered[i].id_name + "\">" +
               "    <span class=\"card-pic\">\n" +
               "      <div class = \"card-pic-crop\" >\n" +
-              "      <img src=\""+ _G_j_pfo_projs[i].img_directory + _G_j_pfo_projs[i].img_cover + "\"/>\n" +
+              "      <img src=\""+ _G_j_pfo_projs_filtered[i].img_directory + _G_j_pfo_projs_filtered[i].img_cover + "\"/>\n" +
               "      </div> \n" +
               "      <div class=\"card-description\">\n" +
               "      <div class=\"card-des-title\">\n" +
-              _G_j_pfo_projs[i].title+
+              _G_j_pfo_projs_filtered[i].title+
               "      </div>\n" +
               "      <div class=\"card-des-explain\">\n" +
-              _G_j_pfo_projs[i].description+
+              _G_j_pfo_projs_filtered[i].description+
               "      </div>\n" +
               "      </div>\n" +
               "    </span>\n" +
               "</div>\n" +
               "</li>";
-
-          $('#pfo-bundle').append(pfo_elem);
       }
+      $('#pfo-bundle').html(pfo_elem).slideDown(400).css({top: -20, opacity: 0}).
+      animate({top: 0, opacity: 1}, 400);
   }
   /*----------------------------------------------------*/
   /* AUTO portfolio modal real-time gen
@@ -82,27 +85,27 @@ $(function(){
   function gen_pfo_modal(_id_name, offset)
   {
       // Use preloaded json to construct page dynamically
-      if(G_j_pfo_projs != null) {
-          var i = indexOfItemInJSON("id_name",_id_name,G_j_pfo_projs);
+      if(G_j_pfo_projs_filtered != null) {
+          var i = indexOfItemInJSON("id_name",_id_name,G_j_pfo_projs_filtered);
           if(i != -1) {
               i = i + offset; //offsetting the selection for prev/next
-              i = i <= -1 ? (G_j_pfo_projs.length-1) : i; //-ve prev
-              i = i >= G_j_pfo_projs.length ? 0 : i; //next is 0
+              i = i <= -1 ? (G_j_pfo_projs_filtered.length-1) : i; //-ve prev
+              i = i >= G_j_pfo_projs_filtered.length ? 0 : i; //next is 0
 
-              G_cur_pfo_id_name = G_j_pfo_projs[i].id_name; //save id_name globally
+              G_cur_pfo_id_name = G_j_pfo_projs_filtered[i].id_name; //save id_name globally
               var modal_html_buffer;
 
-              modal_html_buffer = "<h1>" + G_j_pfo_projs[i].title + "<h1>";
+              modal_html_buffer = "<h1>" + G_j_pfo_projs_filtered[i].title + "<h1>";
               $('#modal-templ-title').html(modal_html_buffer);
 
               modal_html_buffer = "<div class=\"carouselCard\">\n" +
                   "                            <div class=\"owl-demo owl-carousel img-comment\" >";
               //load demo images
-              var j_items = G_j_pfo_projs[i].more_imgs;
+              var j_items = G_j_pfo_projs_filtered[i].more_imgs;
               var j = 0;
               for (j = 0; j < j_items.length; j++) {
-                  modal_html_buffer += "<div><img src=\"" + G_j_pfo_projs[i].img_directory
-                      + j_items[j] + "\"><p>" + G_j_pfo_projs[i].more_imgs_title[j] + "</p></div>\n";
+                  modal_html_buffer += "<div><img src=\"" + G_j_pfo_projs_filtered[i].img_directory
+                      + j_items[j] + "\"><p>" + G_j_pfo_projs_filtered[i].more_imgs_title[j] + "</p></div>\n";
               }
 
               modal_html_buffer += " </div>\n" +
@@ -111,8 +114,8 @@ $(function(){
               $('#modal-templ-imgs').html(modal_html_buffer);
 
               //load details
-              j_terms = G_j_pfo_projs[i].tags;
-              modal_html_buffer = "<p><strong>Position: </strong>" + G_j_pfo_projs[i].position + "</p>\n" +
+              j_terms = G_j_pfo_projs_filtered[i].tags;
+              modal_html_buffer = "<p><strong>Position: </strong>" + G_j_pfo_projs_filtered[i].position + "</p>\n" +
                   "                            <p><strong>Category: </strong>";
               for (j = 0; j < j_terms.length - 1; j++) {
                   modal_html_buffer += j_terms[j] + ", ";
@@ -120,16 +123,16 @@ $(function(){
               modal_html_buffer += j_terms[j];
               modal_html_buffer += "</p>\n" +
                   " <p><strong>View Code: </strong> <i class=\"fa fa-github\" style=\"padding-right:2px;\"></i><a href=\"" +
-                  G_j_pfo_projs[i].code_link + "\" target=\"_blank\">"
-                  + G_j_pfo_projs[i].code_link + "</a></p>\n";
+                  G_j_pfo_projs_filtered[i].code_link + "\" target=\"_blank\">"
+                  + G_j_pfo_projs_filtered[i].code_link + "</a></p>\n";
               $('#modal-templ-details').html(modal_html_buffer);
 
               //load descriptions Paragraphs
               modal_html_buffer = "<h5 class=\"info-font\">Project Description</h5>\n";
-              j_terms = G_j_pfo_projs[i].paras_title;
+              j_terms = G_j_pfo_projs_filtered[i].paras_title;
               for (j = 0; j < j_terms.length; j++) {
                   modal_html_buffer += "<div  class=\"project-head4\"><h4>" + j_terms[j] + "</h4></div>\n";
-                  modal_html_buffer += "<p class=\"project-para\">\n" + G_j_pfo_projs[i].paras[j] + "</p>\n";
+                  modal_html_buffer += "<p class=\"project-para\">\n" + G_j_pfo_projs_filtered[i].paras[j] + "</p>\n";
               }
               $('#modal-templ-description').html(modal_html_buffer);
 
@@ -218,55 +221,26 @@ $("body").niceScroll({
 });
 
   /*----------------------------------------------------*/
-  /* TODO Unknowns !!
+  /* pfo filter selection
   ------------------------------------------------------ */
+  $(document).on('click', '.filter_tag', function () {
+      var $this = $(this);
+      if(!$this.hasClass('activeL'))
+      {
+          $(".activeL").removeClass("activeL");
+          $this.addClass('activeL');
+          tag = $this.attr("datalist");
+          if(tag !== "*")
+          {
+              G_j_pfo_projs_filtered = G_j_pfo_projs.filter(function (item,n){
+                  return (item.tags.indexOf(tag) > -1);
+              });
+          }else
+              G_j_pfo_projs_filtered = G_j_pfo_projs;
 
-// $('.filter li a').click(function(e) {
-//   e.preventDefault();
-//   var $this = $(this);
-//   $('li').removeClass('activeL');
-//   $this.parent().addClass('activeL');
-//   // Filter
-//   var te= document.getElementsByClassName('activeL');
-//   var ac=$( te ).attr("datalist");
-//   // console.log(ac);
-//
-//   var listItems = $("#workList li");
-//   listItems.each(function() {
-//     var dl=$(this).attr("class");
-//     // console.log(dl);
-//
-//     if(ac!='*'){
-//       // console.log(0);
-//       if(dl!=ac){
-//         // console.log(true);
-//         $(this).fadeOut("slow");
-//       }else{
-//         $(this).fadeIn("slow");
-//       }
-//     }else{
-//       $(this).fadeIn("slow");
-//     }
-//   });
-//   //
-//
-//   e.preventDefault();
-// });
-//
-// $('.pageclose').click(function () {
-//   $('.modal').modal('toggle');
-// });
-//
-// $('#lihuili').hover(function(){
-//   $('#edulfs').toggleClass("linebf");
-//   $('#edulfs').toggleClass("linebflihuili");
-// });
-// $('#loyola').hover(function(){
-//   $('#edulfs').toggleClass("linebf");
-//   $('#edulfs').toggleClass("linebfloyola");
-// });
-// $('#uw').hover(function(){
-//   $('#edulfs').toggleClass("linebf");
-//   $('#edulfs').toggleClass("linebfuw");
-// });
+          //regen cards
+          gen_pfo_cards(G_j_pfo_projs_filtered);
+      }
+  });
+
 
